8b8d5fc6732d0ca13f96026e11196f92d37965f0
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-03-24 16:53:27 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(arr){
+    function findWinner(arr){
+      return arr.result === "W"
+    }
+    if(!arr.find(findWinner)){
+      return undefined
+    }
+      return arr.find(findWinner).year
+  }
